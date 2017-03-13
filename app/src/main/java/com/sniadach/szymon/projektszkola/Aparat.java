f3bba1ae92c8a01cc67545f38e58d45395869285
Szymon Śniadach <sz.sniadach@gmail.com>
2017-03-13 18:39:14 +0100

    aparat

---
+++ app/src/main/java/com/sniadach/szymon/projektszkola/Aparat.java
@@ -0,0 +1,18 @@
+package com.sniadach.szymon.projektszkola;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Szymon on 2017-03-13.
+ */
+
+public class Aparat extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.aparat);
+    }
+
+}
--- app/src/main/java/com/sniadach/szymon/projektszkola/MainActivity.java
+++ app/src/main/java/com/sniadach/szymon/projektszkola/MainActivity.java
@@ -1,14 +1,23 @@
 package com.sniadach.szymon.projektszkola;
 
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import java.io.File;
+
 public class MainActivity extends AppCompatActivity {
 
-    Button przycisk, przycisk2, przycisk3, przycisk4;
+    Button przycisk, przycisk2, przycisk3, przycisk4, aparat;
+    final static private int CAPTURE_IMAGE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
         przycisk2 = (Button) findViewById(R.id.button2);
         przycisk3 = (Button) findViewById(R.id.button3);
         przycisk4 = (Button) findViewById(R.id.button4);
+        aparat = (Button) findViewById(R.id.aparat);
 
 
         przycisk.setOnClickListener(new View.OnClickListener() {
@@ -53,5 +63,44 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        aparat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                File mediaDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "ProjektSzkola");
+                File imgFile = new File(mediaDir.getPath() + File.separator + "IMG_" + "ProjektSzkola" + ".jpg");
+                Uri photoURI = FileProvider.getUriForFile(MainActivity.this, BuildConfig.APPLICATION_ID + ".provider", getOutputMediaFile());
+                Uri imgUri = Uri.fromFile(new File(imgFile.getAbsolutePath()));
+                Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                if (Build.VERSION.SDK_INT >= 24) {
+                    intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                    startActivityForResult(intent, CAPTURE_IMAGE);
+                }else{
+                    intent.putExtra(MediaStore.EXTRA_OUTPUT, imgUri);
+                    startActivityForResult(intent, CAPTURE_IMAGE);
+                }
+            }
+        });
+
     }
+
+    private static File getOutputMediaFile() {
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "ProjektSzkola");
+        if (!mediaStorageDir.exists()) {
+            if (!mediaStorageDir.mkdirs()) {
+                Log.d("ProjektSzkola", "problem z utworzeniem katalogu");
+                return null;
+            }
+        }
+
+        File mediaFile;
+        mediaFile = new File(mediaStorageDir.getPath() + File.separator + "IMG_" + "ProjektSzkola" + ".jpg");
+        if (mediaFile.exists()) {
+            mediaFile.delete();
+        }
+        Log.d("Zdjecie1234567 : ", mediaFile.toString());
+
+        return mediaFile;
+    }
+
 }
